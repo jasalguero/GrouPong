d196ba88563f3ce15b69c267b5c76543f3f0ee43
Zbigniew Zabost <zbigniew.zabost@groupon.com>
2012-11-23 23:40:13 +0100

    init db creation script

---
+++ db-creation.sql
@@ -0,0 +1,42 @@
+CREATE TABLE status (
+	id	INTEGER PRIMARY KEY,
+	description	TEXT
+);
+
+
+CREATE TABLE achievement (
+	id		INTEGER PRIMARY KEY,
+	title		TEXT,
+	description	TEXT
+);
+
+
+CREATE TABLE user (
+	id		INTEGER PRIMARY KEY,
+	email		TEXT,
+	password	TEXT,
+	user_name	TEXT,
+	avatar		TEXT,
+	score		INTEGER
+);
+
+
+CREATE TABLE user_achievement (
+	id		INTEGER PRIMARY KEY,
+	user_id		INTEGER,
+	achievement_id	INTEGER
+);
+
+
+CREATE TABLE game (
+        id              INTEGER PRIMARY KEY,
+        user_one        INTEGER,
+        user2           INTEGER,
+        score_user1,    INTEGER,
+        score_user2,    INTEGER,
+        game_date,     DATE,
+        status_id       INTEGER
+);
+
+
+
