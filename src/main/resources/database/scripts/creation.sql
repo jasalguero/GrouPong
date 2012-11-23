39203f430fb8d42fa6bbc564716cb6df7347739d
Zbigniew Zabost <zbigniew.zabost@groupon.com>
2012-11-24 00:30:05 +0100

    finished DB schema
    added script for creation DB and script for input some test data

--- src/main/resources/database/scripts/creation.sql
+++ src/main/resources/database/scripts/creation.sql
@@ -1,18 +1,29 @@
 CREATE TABLE status (
-	id	INTEGER PRIMARY KEY,
+	id		INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
 	description	TEXT
 );
 
+INSERT INTO status (description) VALUES ("Created");
+INSERT INTO status (description) VALUES ("Accepted");
+INSERT INTO status (description) VALUES ("Refused");
+INSERT INTO status (description) VALUES ("Confirmed by user 1");
+INSERT INTO status (description) VALUES ("Confirmed by user 2");
+INSERT INTO status (description) VALUES ("Finished");
+
 
 CREATE TABLE achievement (
-	id		INTEGER PRIMARY KEY,
+	id		INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
 	title		TEXT,
 	description	TEXT
 );
 
+INSERT INTO achievement (title, description) VALUES ("5 in a row", "You have won 5 matches in a row, like a boss!");
+INSERT INTO achievement (title, description) VALUES ("99%", "You won 99% of matches, awesome man!");
+INSERT INTO achievement (title, description) VALUES ("100 games", "Do you play all days long?");
+
 
 CREATE TABLE user (
-	id		INTEGER PRIMARY KEY,
+	id		INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
 	email		TEXT,
 	password	TEXT,
 	user_name	TEXT,
@@ -22,21 +33,23 @@ CREATE TABLE user (
 
 
 CREATE TABLE user_achievement (
-	id		INTEGER PRIMARY KEY,
-	user_id		INTEGER,
-	achievement_id	INTEGER
+	id		INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+	user_id		INTEGER NOT NULL,
+	achievement_id	INTEGER NOT NULL,
+	FOREIGN KEY (user_id)		REFERENCES user(id),
+	FOREIGN KEY (achievement_id)    REFERENCES achievement(id)
 );
 
 
 CREATE TABLE game (
-        id              INTEGER PRIMARY KEY,
-        user1	        INTEGER,
-        user2           INTEGER,
+        id              INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+        user1	        INTEGER NOT NULL,
+        user2           INTEGER NOT NULL,
         score_user1	INTEGER,
         score_user2    	INTEGER,
         game_date	DATE,
-        status_id       INTEGER
+        status_id       INTEGER NOT NULL,
+	FOREIGN KEY (user1)           REFERENCES user(id),
+	FOREIGN KEY (user2)           REFERENCES user(id),
+	FOREIGN KEY (status_id)       REFERENCES status(id)
 );
-
-
-
---
+++ src/main/resources/database/scripts/insert_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO user (email, password, user_name, avatar, score) VALUES ("user1@email.com", "pass1", "user 1", "http://goo.gl/5kLrP", 1000);
+INSERT INTO user (email, password, user_name, avatar, score) VALUES ("user2@email.com", "pass2", "user 2", "http://goo.gl/5kLrP", 2000);
+INSERT INTO user (email, password, user_name, avatar, score) VALUES ("user3@email.com", "pass3", "user 3", "http://goo.gl/5kLrP", 500);
+
+INSERT INTO user_achievement (user_id, achievement_id) VALUES (1, 1);
+INSERT INTO user_achievement (user_id, achievement_id) VALUES (1, 2);
+INSERT INTO user_achievement (user_id, achievement_id) VALUES (1, 3);
+INSERT INTO user_achievement (user_id, achievement_id) VALUES (2, 1);
+
+
+INSERT INTO game (user1, user2, score_user1, score_user2, game_date, status_id) VALUES (1, 2, null, null, '2012-11-01', 1);
+INSERT INTO game (user1, user2, score_user1, score_user2, game_date, status_id) VALUES (2, 3, null, null, '2012-11-01', 2);
+INSERT INTO game (user1, user2, score_user1, score_user2, game_date, status_id) VALUES (3, 1, 0, 3, '2012-11-01', 3);
+INSERT INTO game (user1, user2, score_user1, score_user2, game_date, status_id) VALUES (3, 2, 1, 2, '2012-11-01', 4);
+INSERT INTO game (user1, user2, score_user1, score_user2, game_date, status_id) VALUES (1, 3, 0, 3, '2012-11-01', 5);
+
