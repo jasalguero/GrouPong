64462ce9afff72e650889df500ae752e94a58552
Zbigniew Zabost <zbigniew.zabost@groupon.com>
2012-11-24 23:40:29 +0100

    fixed avatars urls again

--- src/main/resources/database/scripts/insert_data.sql
+++ src/main/resources/database/scripts/insert_data.sql
@@ -1,7 +1,7 @@
-INSERT INTO user (email, password, user_name, avatar, score) VALUES ("zbigniew@groupon.com", "rollback", "Zbigniew Zabost", "/images/avatars/diver_avatar.png", 1700);
-INSERT INTO user (email, password, user_name, avatar, score) VALUES ("bart@groupon.com", "rollback", "Bart B. aka Bobby", "/images/avatars/dad_avatar.png", 2000);
-INSERT INTO user (email, password, user_name, avatar, score) VALUES ("jose@groupon.com", "rollback", "Jose A. Salguero", "/images/avatars/assassin_avatar.png", 1200);
-INSERT INTO user (email, password, user_name, avatar, score) VALUES ("guest@groupon.com", "rollback", "Santa Claus", "/images/avatars/designer_avatar.png", 1000);
+INSERT INTO user (email, password, user_name, avatar, score) VALUES ("zbigniew@groupon.com", "rollback", "Zbigniew Zabost", "images/avatars/diver_avatar.png", 1700);
+INSERT INTO user (email, password, user_name, avatar, score) VALUES ("bart@groupon.com", "rollback", "Bart B. aka Bobby", "images/avatars/dad_avatar.png", 2000);
+INSERT INTO user (email, password, user_name, avatar, score) VALUES ("jose@groupon.com", "rollback", "Jose A. Salguero", "images/avatars/assassin_avatar.png", 1200);
+INSERT INTO user (email, password, user_name, avatar, score) VALUES ("guest@groupon.com", "rollback", "Santa Claus", "images/avatars/designer_avatar.png", 1000);
 
 
 INSERT INTO game (user1, user2, score_user1, score_user2, game_date, status_id) VALUES (1, 2, 2, 1, '1353765246620', 6);
