c45ca3ea4f789b7ccb5c9ffcddd3bf236f4a13c0
Bartosz Bednarek <bbednarek@groupon.com>
2012-11-25 00:22:46 +0100

    score service

---
+++ src/main/java/de/groupon/hcktn/groupong/service/impl/ScoreServiceImpl.java
@@ -0,0 +1,16 @@
+package de.groupon.hcktn.groupong.service.impl;
+
+import de.groupon.hcktn.groupong.domain.response.MatchDTO;
+import de.groupon.hcktn.groupong.model.entity.Match;
+import de.groupon.hcktn.groupong.service.ScoreService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScoreServiceImpl implements ScoreService {
+
+
+    @Override
+    public void score(Match match, MatchDTO matchDTO) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
