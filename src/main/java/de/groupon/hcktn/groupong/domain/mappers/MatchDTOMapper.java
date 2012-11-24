c1ff1bc5b97cb52ec32f84796ef939e7ebe1f1f3
Bartosz Bednarek <bbednarek@groupon.com>
2012-11-24 20:34:10 +0100

    Fixing mapper

--- src/main/java/de/groupon/hcktn/groupong/domain/mappers/MatchDTOMapper.java
+++ src/main/java/de/groupon/hcktn/groupong/domain/mappers/MatchDTOMapper.java
@@ -1,6 +1,7 @@
 package de.groupon.hcktn.groupong.domain.mappers;
 
 import de.groupon.hcktn.groupong.domain.response.MatchDTO;
+import de.groupon.hcktn.groupong.domain.response.StatusDTO;
 import de.groupon.hcktn.groupong.model.entity.Match;
 import de.groupon.hcktn.groupong.service.StatusService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,13 @@ public class MatchDTOMapper extends BaseDTOMapper {
         matchDTO.setUser2Id(match.getUser2Id());
         matchDTO.setScoreUser1(match.getScoreUser1());
         matchDTO.setScoreUser2(match.getScoreUser2());
-        matchDTO.setDate(formatTime(Long.valueOf(match.getMatchDate())));
+        final String dateString = match.getMatchDate();
+        final Long dateLong = Long.valueOf(dateString);
+        matchDTO.setDate(formatTime(dateLong));
         if (match.getStatusId() != null ) {
-            matchDTO.setStatus(statusService.fetchStatus(match.getStatusId()).getDescription());
+            StatusDTO statusDTO = statusService.fetchStatus(match.getStatusId());
+            matchDTO.setStatus(statusDTO.getDescription());
+            matchDTO.setStatusId(statusDTO.getId());
         }
         return matchDTO;
     }
@@ -47,7 +52,9 @@ public class MatchDTOMapper extends BaseDTOMapper {
             match.setScoreUser2(matchDTO.getScoreUser2());
         }
         if (matchDTO.getDate() != null) {
-            match.setMatchDate(formatTime(Long.valueOf(matchDTO.getDate())));
+            final String matchDTODate = matchDTO.getDate();
+            final Long dateLong = formatTime(matchDTODate);
+            match.setMatchDate(dateLong.toString());
         }
         if (matchDTO.getStatusId() != null) {
             match.setStatusId(matchDTO.getStatusId());
